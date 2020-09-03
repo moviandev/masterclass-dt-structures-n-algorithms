a5985bf1d101280745d4000b1574b2afdb5aa63e
Matheus Viana <matheus_o_viana@hotmail.com>
2020-09-03 20:35:57 -0300

    feat: arrays trhough the lens of big-O

---
+++ analyzing-performance-arrays-objects/arrays.js
@@ -0,0 +1,15 @@
+// Ordered DS
+
+let names = ["Michael", "Melissa", "Andrea"];
+
+let values = [ture, {}, [], 2, "awesome"];
+
+/*
+
+  names[0] === O(1)
+  names.push('') === O(1) 
+  names.pop() === O(1)
+  names.shift('') === O(n)
+  names.unshift() === O(n)
+
+*/
