aaa27a7cc1ded21cb6da2cb22acc82574d72fde3
Matheus Viana <matheus_o_viana@hotmail.com>
2020-09-03 20:21:55 -0300

    feat: objects trhough the lens of big-O

---
+++ analyzing-performance-arrays-objects/objects.js
@@ -0,0 +1,12 @@
+// Unordered DS, key value pairs
+
+let instructor = {
+  firstname: "Kelly",
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3],
+};
+
+// Object.keys === O(n) time
+// Object.values === O(n) time
+// Object.entries === O(n) time
+// Object.hasOwnProperty === O(1) time
